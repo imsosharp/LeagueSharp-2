0318641dcf6bcf6c2638e91a62021922bc17c5a3
h3h3 <h3h3.rc@gmail.com>
2014-09-19 06:08:09 +0200

    Update Killability
    Rework

--- Killability/Killability/Program.cs
+++ Killability/Killability/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Killability.Properties;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-using Color = System.Drawing.Color;
 
 namespace Killability
 {
@@ -29,12 +29,12 @@ namespace Killability
 
             _items = new List<Items.Item>
             {
+                new Items.Item(3128, 750), // Deathfire Grasp
                 new Items.Item(3077, 400), // Tiamat
                 new Items.Item(3074, 400), // Ravenous Hydra
                 new Items.Item(3146, 700), // Hextech Gunblade
                 new Items.Item(3144, 450), // Bilgewater Cutlass
-                new Items.Item(3153, 450), // Blade of the Ruined King
-                new Items.Item(3128, 750)  // Deathfire Grasp
+                new Items.Item(3153, 450)  // Blade of the Ruined King
             };
 
             _spells = new List<Spell>
@@ -46,19 +46,66 @@ namespace Killability
             };
 
             _config = new Menu("Killability", "Killability", true);
-            _config.AddItem(new MenuItem("color", "Color").SetValue(Color.Aqua));
-            _config.AddItem(new MenuItem("combo", "Show Combo").SetValue(true));
+            _config.AddItem(new MenuItem("icon", "Show Icon").SetValue(true));
+            _config.AddItem(new MenuItem("text", "Show Text").SetValue(true));
             _config.AddToMainMenu();
 
-            Drawing.OnDraw += Drawing_OnDraw;
+            InitDrawing();
+
             Game.PrintChat("Killability by h3h3 loaded.");
         }
 
-        private Tuple<double, List<Tuple<DamageLib.SpellType, DamageLib.StageType>>> GetComboDamage(Obj_AI_Hero target)
+        private void InitDrawing()
         {
-            var combo = new List<Tuple<DamageLib.SpellType, DamageLib.StageType>>();
 
-            foreach (var item in _items.Where(item => item.IsReady()))
+            foreach (var h in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsEnemy))
+            {
+                var hero = h;
+                var sprite = new Render.Sprite(Resources.Skull, hero.HPBarPosition);
+                sprite.Scale = new Vector2(0.08f, 0.08f);
+                sprite.Add(0);
+                sprite.PositionUpdate += () => new Vector2(hero.HPBarPosition.X + 140, hero.HPBarPosition.Y + 10);
+                sprite.VisibleCondition += s =>
+                    Render.OnScreen(Drawing.WorldToScreen(hero.Position)) &&
+                    GetComboResult(hero).IsKillable &&
+                    _config.Item("icon").GetValue<bool>();
+
+                var text = new Render.Text(hero.HPBarPosition, "-", 18, new ColorBGRA(255, 255, 255, 255));
+                text.Add(1);
+                text.OutLined = true;
+                text.PositionUpdate += () => new Vector2(hero.HPBarPosition.X + 20, hero.HPBarPosition.Y + 50);
+                text.VisibleCondition += s =>
+                    Render.OnScreen(Drawing.WorldToScreen(hero.Position)) &&
+                    _config.Item("text").GetValue<bool>();
+                text.TextUpdate += () => GetComboResult(hero).Text;
+            }
+        }
+
+        private class ComboResult
+        {
+            public List<DamageLib.SpellType> Spells { get; set; }
+            public bool IsKillable { get; set; }
+            public double ManaCost { get; set; }
+            public string Text { get; set; }
+
+            public ComboResult()
+            {
+                Spells = new List<DamageLib.SpellType>();
+            }
+        }
+
+        private ComboResult GetComboResult(Obj_AI_Hero target)
+        {
+            if (!target.IsValidTarget())
+                return new ComboResult();
+
+            var combo = new List<Tuple<DamageLib.SpellType, DamageLib.StageType>>();
+            var result = new ComboResult();
+            var comboMana = 0f;
+
+            foreach (var item in _items
+                .Where(item => item.IsReady())
+                .TakeWhile(item => !DamageLib.IsKillable(target, combo)))
             {
                 switch (item.Id)
                 {
@@ -86,72 +133,50 @@ namespace Killability
                         DamageLib.SpellType.BOTRK,
                         DamageLib.StageType.Default)); break;
                 }
-
-                if (DamageLib.IsKillable(target, combo))
-                    break;
             }
 
-            foreach (var spell in _spells.Where(spell => spell.IsReady()))
+            foreach (var spell in _spells
+                .Where(spell => spell.Level > 0)
+                .TakeWhile(item => !DamageLib.IsKillable(target, combo)))
             {
                 switch (spell.Slot)
                 {
                     case SpellSlot.Q: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.Q, 
+                        DamageLib.SpellType.Q,
                         DamageLib.StageType.Default)); break;
 
                     case SpellSlot.W: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.W, 
+                        DamageLib.SpellType.W,
                         DamageLib.StageType.Default)); break;
 
                     case SpellSlot.E: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.E, 
+                        DamageLib.SpellType.E,
                         DamageLib.StageType.Default)); break;
 
                     case SpellSlot.R: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.R, 
+                        DamageLib.SpellType.R,
                         DamageLib.StageType.Default)); break;
                 }
 
-                if(DamageLib.IsKillable(target, combo))
-                    break;
+                comboMana += spell.Instance.ManaCost;
             }
 
-            if (_ignite != null && _ignite.State == SpellState.Ready)
+            if (_ignite != null && _ignite.State == SpellState.Ready && !DamageLib.IsKillable(target, combo))
                 combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
                     DamageLib.SpellType.IGNITE,
                     DamageLib.StageType.Default));
 
-            return new Tuple<double, List<Tuple<DamageLib.SpellType, DamageLib.StageType>>>(
-                DamageLib.GetComboDamage(target, combo), 
-                combo);
-        }
+            result.Spells.AddRange(combo.Select(c => c.Item1));
+            result.IsKillable = DamageLib.IsKillable(target, combo) && ObjectManager.Player.Mana > comboMana;
+            result.ManaCost = comboMana;
 
-        private bool IsOnScreen(Vector3 v)
-        {
-            var pos = Drawing.WorldToScreen(v);
-            return pos.X < Drawing.Width && pos.Y < Drawing.Width;
-        }
+            if (result.IsKillable)
+                result.Text = string.Join("/", result.Spells);
 
-        private void Drawing_OnDraw(EventArgs args)
-        {
-            foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsValidTarget() && IsOnScreen(h.Position)))
-            {
-                var dmg = GetComboDamage(target);
+            if (ObjectManager.Player.Mana < comboMana)
+                result.Text = "LOW MANA";
 
-                if (dmg.Item1 > target.Health * 0.9)
-                {
-                    var pos = Drawing.WorldToScreen(target.Position);
-                    var text = "Killable";
-                    var textOffset = Drawing.GetTextExtent(text).Width / 2;
-                    var combo = string.Join("/", dmg.Item2.Select(d => d.Item1));
-                    var comboOffset = Drawing.GetTextExtent(combo).Width / 2;
-
-                    Drawing.DrawText(pos.X - textOffset, pos.Y, _config.Item("color").GetValue<Color>(), text);
-
-                    if (_config.Item("combo").GetValue<bool>())
-                        Drawing.DrawText(pos.X - comboOffset, pos.Y + 15, _config.Item("color").GetValue<Color>(), combo);
-                }
-            }
+            return result;
         }
     }
 }
