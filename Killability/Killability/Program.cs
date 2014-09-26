7001ac397c5e194996b0de83ac3b778a1599a736
h3h3 <h3h3.rc@gmail.com>
2014-09-26 23:35:30 +0200

    Update 4.17

--- Killability/Killability/Program.cs
+++ Killability/Killability/Program.cs
@@ -15,9 +15,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Killability.Properties;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
@@ -42,7 +44,7 @@ namespace Killability
         public KillDrawer()
         {
             _ignite = ObjectManager.Player.SummonerSpellbook.Spells.FirstOrDefault(x => x.Name.ToLower() == "summonerdot");
-
+            _spells = new List<Spell>();
             _items = new List<Items.Item>
             {
                 new Items.Item(3128, 750), // Deathfire Grasp
@@ -53,13 +55,41 @@ namespace Killability
                 new Items.Item(3153, 450)  // Blade of the Ruined King
             };
 
-            _spells = new List<Spell>
+            try
             {
-                new Spell(SpellSlot.Q),
-                new Spell(SpellSlot.W),
-                new Spell(SpellSlot.E),
-                new Spell(SpellSlot.R)
-            };
+                new Spell(SpellSlot.Q).GetDamage(ObjectManager.Player);
+                _spells.Add(new Spell(SpellSlot.Q));
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                new Spell(SpellSlot.W).GetDamage(ObjectManager.Player);
+                _spells.Add(new Spell(SpellSlot.W));
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                new Spell(SpellSlot.E).GetDamage(ObjectManager.Player);
+                _spells.Add(new Spell(SpellSlot.E));
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                new Spell(SpellSlot.R).GetDamage(ObjectManager.Player);
+                _spells.Add(new Spell(SpellSlot.R));
+            }
+            catch (Exception)
+            {
+            }
 
             _config = new Menu("Killability", "Killability", true);
             _config.AddItem(new MenuItem("icon", "Show Icon").SetValue(true));
@@ -86,9 +116,7 @@ namespace Killability
                 //    _config.Item("icon").GetValue<bool>();
 
                 var text = new Render.Text("", hero, new Vector2(20, 50), 18, new ColorBGRA(255, 255, 255, 255));
-                text.VisibleCondition += s =>
-                    Render.OnScreen(Drawing.WorldToScreen(hero.Position)) &&
-                    _config.Item("text").GetValue<bool>();
+                text.VisibleCondition += s => Render.OnScreen(Drawing.WorldToScreen(hero.Position)) && _config.Item("text").GetValue<bool>();
                 text.TextUpdate += () =>
                 {
                     var result = GetComboResult(hero);
@@ -101,14 +129,14 @@ namespace Killability
 
         private class ComboResult
         {
-            public List<DamageLib.SpellType> Spells { get; set; }
+            public List<SpellSlot> Spells { get; set; }
             public bool IsKillable { get; set; }
             public double ManaCost { get; set; }
             public string Text { get; set; }
 
             public ComboResult()
             {
-                Spells = new List<DamageLib.SpellType>();
+                Spells = new List<SpellSlot>();
             }
         }
 
@@ -117,81 +145,55 @@ namespace Killability
             if (!target.IsValidTarget())
                 return new ComboResult();
 
-            var combo = new List<Tuple<DamageLib.SpellType, DamageLib.StageType>>();
+            var player = ObjectManager.Player;
             var result = new ComboResult();
             var comboMana = 0f;
+            var comboDmg = 0d;
 
-            foreach (var item in _items
-                .Where(item => item.IsReady())
-                .TakeWhile(item => !DamageLib.IsKillable(target, combo)))
+            foreach (var item in _items.Where(item => item.IsReady()))
             {
                 switch (item.Id)
                 {
-                    case 3128: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.DFG,
-                        DamageLib.StageType.FirstDamage)); break;
-
-                    case 3077: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.TIAMAT,
-                        DamageLib.StageType.Default)); break;
-
-                    case 3074: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.HYDRA,
-                        DamageLib.StageType.Default)); break;
-
-                    case 3146: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.HEXGUN,
-                        DamageLib.StageType.Default)); break;
-
-                    case 3144: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.BILGEWATER,
-                        DamageLib.StageType.Default)); break;
-
-                    case 3153: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.BOTRK,
-                        DamageLib.StageType.Default)); break;
+                    case 3128: comboDmg += player.GetItemDamage(target, Damage.DamageItems.Dfg); break;
+                    case 3077: comboDmg += player.GetItemDamage(target, Damage.DamageItems.Tiamat); break;
+                    case 3074: comboDmg += player.GetItemDamage(target, Damage.DamageItems.Hydra); break;
+                    case 3146: comboDmg += player.GetItemDamage(target, Damage.DamageItems.Hexgun); break;
+                    case 3144: comboDmg += player.GetItemDamage(target, Damage.DamageItems.Bilgewater); break;
+                    case 3153: comboDmg += player.GetItemDamage(target, Damage.DamageItems.Botrk); break;
                 }
+
+                if (comboDmg > target.Health)
+                    break;
             }
 
-            foreach (var spell in _spells
-                .Where(spell => spell.Level > 0)
-                .TakeWhile(item => !DamageLib.IsKillable(target, combo)))
+            foreach (var spell in _spells.Where(spell => spell.Level > 0))
             {
-                switch (spell.Slot)
+                try
                 {
-                    case SpellSlot.Q: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.Q,
-                        DamageLib.StageType.Default)); break;
+                    comboDmg += spell.GetDamage(target, 1);
+                    comboMana += spell.Instance.ManaCost;
+                    result.Spells.Add(spell.Slot);
 
-                    case SpellSlot.W: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.W,
-                        DamageLib.StageType.Default)); break;
-
-                    case SpellSlot.E: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.E,
-                        DamageLib.StageType.Default)); break;
-
-                    case SpellSlot.R: combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                        DamageLib.SpellType.R,
-                        DamageLib.StageType.Default)); break;
+                    if (comboDmg > target.Health)
+                        break;
                 }
-
-                comboMana += spell.Instance.ManaCost;
+                catch
+                {
+                }
             }
 
-            if (_ignite != null && _ignite.State == SpellState.Ready && !DamageLib.IsKillable(target, combo))
-                combo.Add(new Tuple<DamageLib.SpellType, DamageLib.StageType>(
-                    DamageLib.SpellType.IGNITE,
-                    DamageLib.StageType.Default));
+            if (_ignite != null && _ignite.State == SpellState.Ready && target.Health >  comboDmg)
+            {
+                comboDmg += player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite);
+            }
 
-            result.Spells.AddRange(combo.Select(c => c.Item1));
-            result.IsKillable = DamageLib.IsKillable(target, combo) && ObjectManager.Player.Mana > comboMana;
+            result.IsKillable = comboDmg > target.Health && player.Mana > comboMana;
             result.ManaCost = comboMana;
 
             if (result.IsKillable)
                 result.Text = string.Join("/", result.Spells);
 
-            if (ObjectManager.Player.Mana < comboMana)
+            if (player.Mana < comboMana)
                 result.Text = "LOW MANA";
 
             return result;
