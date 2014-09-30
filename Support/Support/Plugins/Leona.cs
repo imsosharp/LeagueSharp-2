763276cd0c30a820995a379225bfa6f3a24f0723
h3h3 <h3h3.rc@gmail.com>
2014-09-30 05:56:20 +0200

    Leona Update

--- Support/Support/Plugins/Leona.cs
+++ Support/Support/Plugins/Leona.cs
@@ -51,7 +51,10 @@ namespace Support.Plugins
 
         public override void AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
         {
-            if (!unit.IsMe && !(unit is Obj_AI_Hero))
+            if (!unit.IsMe)
+                return;
+
+            if (!(target is Obj_AI_Hero) && !target.Name.Contains("Ward")) 
                 return;
 
             if (Q.Cast())
