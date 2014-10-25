cffb5bf208d24407b1515e03ee754d9d14e24f33
h3h3 <h3h3.rc@gmail.com>
2014-10-25 06:37:18 +0200

    Support Zyra enabled

--- Support/Support/Properties/AssemblyInfo.cs
+++ Support/Support/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.5.3")]
-[assembly: AssemblyFileVersion("2.0.5.3")]
+[assembly: AssemblyVersion("2.0.5.5")]
+[assembly: AssemblyFileVersion("2.0.5.5")]
