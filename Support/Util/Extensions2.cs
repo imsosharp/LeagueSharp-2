ddd896c6c0764e5160bcec3231ed9699906667dc
h3h3 <h3h3.rc@gmail.com>
2014-12-04 19:05:01 +0100

    Update for Common 1.0.0.10 fix2

--- Support/Util/Extensions2.cs
+++ Support/Util/Extensions2.cs
@@ -37,7 +37,7 @@ namespace Support.Util
         Bruiser
     }
 
-    public static class Extensions
+    public static class Extensions2
     {
         private static readonly string[] Ap =
         {
